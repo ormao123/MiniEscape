983adc74811244e489e486628681f73181f8edbe
germansk8ter <minimoney1@live.com>
2014-08-23 16:59:35 -0400

    additions

--- MiniEscape.lua
+++ MiniEscape.lua
@@ -1,4 +1,31 @@
 local version = 1.0
+local FLASH_RANGE = 400
+
+local delayedActions, delayedActionsExecuter = {}, nil
+local DelayActionEx = 
+	function(func, delay, args)
+		if (not delayedActionsExecuter) then
+			function delayedActionsExecuter()
+				for t, funcs in pairs(delayedActions) do
+					if (t <= os.clock()) then
+						for _, f in ipairs(funcs) do 
+							f.func(table.unpack(f.args or {})) 
+						end
+						delayedActions[t] = nil
+					end
+				end
+			end
+
+			AddTickCallback(delayedActionsExecuter)
+		end
+		local t = os.clock() + (delay or 0)
+		if (delayedActions[t]) then 
+			table.insert(delayedActions[t], { func = func, args = args })
+		else
+			delayedActions[t] = { { func = func, args = args } }
+		end
+		return delayedActions, t, #t
+	end
 
 AddLoadCallback(
 	function()
@@ -17,9 +44,11 @@ AddLoadCallback(
 			return
 		end
 		Config = scriptConfig("MiniEscape", "MiniEscape")
-		Config = Config:addParam("flash", "Escape!", SCRIPT_PARAM_ONKEYTOGGLE, false, GetKey("V"))
-		Config = Config:addParam("duration", "Duration that Escape Remains on After Press", SCRIPT_PARAM_SLICE, 3, 1, 10)
-		Config = Config:addParam("autoLowHP", "Auto Escape on Low HP", SCRIPT_PARAM_ONOFF, false)
+		Config:addParam("flash", "Escape!", SCRIPT_PARAM_ONKEYTOGGLE, false, GetKey("V"))
+		Config:addParam("duration", "Duration that Escape Remains on After Press", SCRIPT_PARAM_SLICE, 3, 1, 10)
+		Config:addParam("autoLowHP", "Auto Escape on Low HP", SCRIPT_PARAM_ONOFF, false)
+		Config:addParam("dist", "Distance Required from Flash Position to myHero to Flash Away", SCRIPT_PARAM_SLICE, 100, 10, 500)
+		Config:addParam("Angle", "Angle Required Between Flash Position and myHero to Flash Away", SCRIPT_PARAM_SLICE, 30, 0, 90)
 		print("MiniEscape Loaded")
 	end)
 
@@ -31,7 +60,12 @@ AddMsgCallback(
 			end
 			if (msg == KEY_DOWN) then
 				if (not buttonDown and Config.flash) then
-					DelayAction(
+					if (#delayedActions ~= 0) then
+						for i, v in ipairs(delayedActions) do
+							table.remove(delayedActions, i)
+						end
+					end
+					DelayActionEx(
 						function()
 							if (Config.flash ~= false) then
 								Config.flash = false
@@ -45,7 +79,7 @@ AddMsgCallback(
 
 AddRecvPacketCallback(
 	function(p)
-		if (flashSlot ~= nil and (Config.flash or Config.autoLowHP) and p.header == 181) then
+		if (flashSlot ~= nil and (Config.flash or Config.autoLowHP) and p.header == 0xB5) then
 			p.pos = 1
 			local blinker = objManager:GetObjectByNetworkId(p:DecodeF())
 			if (blinker ~= nil and blinker.valid and blinker.type == myHero.type and blinker.visible and blinker.team ~= myHero.team) then
@@ -69,7 +103,14 @@ AddRecvPacketCallback(
 
 					local vStartPos = checkData(startX, startY, startZ, blinker.visionPos)
 					local vEndPos = Vector(x, y, z)
-					local pos = vStartPos - (vStartPos - vEndPos):normalized() * 475
+					local checkFinalPos = 
+						function(startPos, endPos)
+							if (GetDistance(startPos, endPos) <= FLASH_RANGE) then
+								return endPos
+							end
+							return (startPos - (startPos - endPos):normalized() * FLASH_RANGE)
+						end
+					local pos = checkFinalPos(vStartPos, vEndPos)
 					local towardsMe = 
 						function(pos)
 							local myPos = Vector(myHero.visionPos.x, myHero.visionPos.y, myHero.visionPos.z)
@@ -87,16 +128,17 @@ AddRecvPacketCallback(
 						end
 					if (towardsMe(pos)) then
 						local checkHP =
-							function(enemy)
-								
+							function()
+								if ((myHero.maxHealth / myHero.health) < HP_LIMIT) then
+									return true
+								end
 								return Config.flash
 							end
 
-						if (checkHP(blinker)) then
-
+						if (checkHP()) then
+							local flashpos = (Vector(myHero.visionPos) - pos):normalized() * FLASH_RANGE
+							Packet("S_CAST", {spellId = flashSlot, toX = flashpos.x, toY = flashpos.z, fromX = flashpos.x, fromY = flashpos.z}):send()
 						end
-						local flashpos = (Vector(myHero.visionPos) - pos):normalized() * 475
-						Packet("S_CAST", {spellId = flashSlot, toX = flashpos.x, toY = flashpos.z, fromX = flashpos.x, fromY = flashpos.z}):send()
 					end
 				end
 			end
